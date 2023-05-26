f0b5039aad5219286a1422c9caaabe5864ae6cff
kdevivanco <kdevivanco@gmail.com>
2023-05-26 07:52:52 -0500

    primer test

---
+++ README.md
@@ -0,0 +1,6 @@
+# Instrucciones para correr en la terminal 
+
+## 1. 
+terminal ~ make
+terminal ~ ./MyProject
+
---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+    #include <opencv2/imgcodecs.hpp>
+    #include <opencv2/highgui.hpp>
+    #include <opencv2/imgproc.hpp>
+    #include <iostream>
+
+    using namespace cv;
+    using namespace std;
+
+
+   /////////////////  Images  //////////////////////
+
+    int main() {
+
+        string path = "/Users/macbookpro/Desktop/UTEC/OpenCV/opencv3test/Resources/test.png";
+        Mat img = imread(path);
+        imshow("Image", img);
+        waitKey(0);
+        return 0;
+    }
