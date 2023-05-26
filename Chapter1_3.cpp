c3acb8167934a44bda6a49c6a3488f58add0a3be
kdevivanco <kdevivanco@gmail.com>
2023-05-26 13:45:16 -0500

    Importing Webcam

---
+++ Chapter1_3.cpp
@@ -0,0 +1,29 @@
+
+//webcam test
+
+//Reference: https://www.youtube.com/watch?v=2FYm3GOonhk
+//Min 30
+
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+///// Importing webcam //// 
+
+int main(){
+    //cap is the name of the video
+    VideoCapture cap(0); // If only one camara attached, else camera id 
+    //Then we will read the images one by one
+    Mat img;
+
+    while(true){
+        cap.read(img);
+        imshow("Image",img);
+        
+    waitKey(1);
+    }
+}
